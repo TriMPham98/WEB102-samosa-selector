5d1cf5222a52cd048254ac4d328bf96fcf97093d
Tri <trimpham98@gmail.com>
2024-03-19 21:46:02 -0700

    Format the time

--- somosa-selector/src/App.jsx
+++ somosa-selector/src/App.jsx
@@ -1,12 +1,7 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
+  return <div className="App"></div>;
+};
 
-  return (
-    <div className="App">
-
-    </div>
-  )
-}
-
-export default App
+export default App;
