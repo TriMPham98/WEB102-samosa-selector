a8c6ca334bc7ab60cb3a1dd8f863009797bd9049
Tri <trimpham98@gmail.com>
2024-03-20 09:33:56 -0700

    Add divs and h1 and h2 tags

--- somosa-selector/src/App.jsx
+++ somosa-selector/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App"></div>;
+  return <div className="App">
+    <div className="header">
+      <h1>"Somosa Selector"</h1>
+      <h2>"Count:"</h2>
+      <img className="samosa" src=""></img> // TODO: Find image of somosa
+    </div>
+  </div>;
 };
 
 export default App;
