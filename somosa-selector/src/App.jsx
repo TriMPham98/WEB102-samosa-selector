dfa4f5825beddd77d45b0814a81b1d2e222f0978
Tri <trimpham98@gmail.com>
2024-03-22 13:17:22 -0700

    Add updateCount function on samosa click

--- somosa-selector/src/App.jsx
+++ somosa-selector/src/App.jsx
@@ -3,15 +3,21 @@ import { useState } from "react";
 
 const App = () => {
   const [count, setCount] = useState(0);
+
+  const updateCount = (multiplier = 1) => {
+    setCount(count + multiplier);
+  };
+
   return (
     <div className="App">
       <div className="header">
         <h1>Somosa Selector</h1>
-        <h2>Count:</h2>
+        <h2>Count: {count}</h2>
         <img
           className="samosa"
           src="https://cdn.shopify.com/s/files/1/1785/5627/t/60/assets/origins_of_chicken_samosa-1697398529680_400x.jpg?v=1697398530"
-          alt="Samosa"></img>
+          alt="Samosa"
+          onClick={() => updateCount(1)}></img>
       </div>
     </div>
   );
