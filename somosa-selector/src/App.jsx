035d30659d287cf57114fd43976e3bdae8c8f273
Tri <trimpham98@gmail.com>
2024-03-22 00:05:41 -0700

    Add useState import

--- somosa-selector/src/App.jsx
+++ somosa-selector/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
+import { useState } from "react";
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className="App">
       <div className="header">
