b2fae77e93b9073692dd4bc9eccbe0e3d7628b11
Tri <trimpham98@gmail.com>
2024-03-21 23:55:12 -0700

    Format the time

--- somosa-selector/src/App.jsx
+++ somosa-selector/src/App.jsx
@@ -1,13 +1,18 @@
 import "./App.css";
 
 const App = () => {
-  return <div className="App">
-    <div className="header">
-      <h1>Somosa Selector</h1>
-      <h2>Count:</h2>
-      <img className="samosa" src="https://cdn.shopify.com/s/files/1/1785/5627/t/60/assets/origins_of_chicken_samosa-1697398529680_400x.jpg?v=1697398530" alt="Samosa"></img>
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Somosa Selector</h1>
+        <h2>Count:</h2>
+        <img
+          className="samosa"
+          src="https://cdn.shopify.com/s/files/1/1785/5627/t/60/assets/origins_of_chicken_samosa-1697398529680_400x.jpg?v=1697398530"
+          alt="Samosa"></img>
+      </div>
     </div>
-  </div>;
+  );
 };
 
 export default App;
